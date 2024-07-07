9d382e5540719c4cb7c405bad1f2817561d6036c
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-07-08 00:41:00 +0900

    settime 할 수 있는 코드 추가

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import TimerWrapper from "./timer.style";
 
 const Timer = () => {
+  const [time, setTime] = useState(0);
+
+  const onChange = (e) => {
+    console.log("onChange");
+    setTime(e.target.value);
+  };
+
+  const startTimer = () => {
+    console.log("startTimer: " + time);
+  };
+
   return (
     <TimerWrapper>
       <label>타이머를 설정할 시간을 입력하세요</label>
-      <input type="number"></input>
-      <button>설정하기</button>
+      <input type="number" onChange={onChange} value={time} />
+      <button onClick={startTimer}>설정하기</button>
     </TimerWrapper>
   );
 };
